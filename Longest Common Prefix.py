812d55f4057487835b720cdee1e30720efc9cc7a
Shir Ezra <83343641+ezrashir@users.noreply.github.com>
2023-09-19 13:55:58 +0300

    Add files via upload

---
+++ Longest Common Prefix.py
@@ -0,0 +1,34 @@
+# Link to problem:
+    # https://leetcode.com/problems/longest-common-prefix/
+
+
+def longestCommonPrefix(strs):
+    if len(strs) == 1:
+        return strs[0]
+    strs.sort(key=len) 
+    word = strs[0]
+    comb = []
+    
+    for i in range(1, len(word)+1):
+            comb.append(word[0:i])
+    comb.sort(key=len, reverse=True)
+
+    well = False
+    for i in range(len(comb)):
+        for k in range(1, len(strs)):
+            if strs[k][0:len(comb[i])] == comb[i]:
+                well = True  
+                continue      
+            else:
+                well = False
+                break
+        if well:
+            return comb[i]
+        else:
+            continue
+    return ""
+
+
+# Example:
+strs = ["fdfvsrd", "fdfv", "fdfvjhefdkaejh", "fdfvtsdsa"]
+print(longestCommonPrefix(strs))
---
+++ Roman to Integer.py
@@ -0,0 +1,30 @@
+# Link to problem:
+    # https://leetcode.com/problems/roman-to-integer/
+
+
+
+class Solution():
+    def romanToInt(self, s:str):
+        dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        temp = 0
+        for i in range(len(s)-1):
+            if dict[s[i]] >= dict[s[i+1]]:
+                temp += dict[s[i]]
+            if dict[s[i]] < dict[s[i+1]]:
+                temp -= dict[s[i]]
+        temp += dict[s[-1]]
+        return temp
+
+
+# Example:
+str = 'CCCXLIV'
+roman = Solution()
+print(roman.romanToInt(s=str))
