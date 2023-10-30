0e897d2c424af5869e9494d66ba0bd5f563fdb0a
Shir Ezra <83343641+ezrashir@users.noreply.github.com>
2023-10-30 13:48:58 +0200

    Add files via upload

---
+++ Sqrt(x).py
@@ -0,0 +1,26 @@
+# Link to submission:
+    # https://leetcode.com/problems/sqrtx/submissions/1087507621/
+
+# Link to problem:
+    # https://leetcode.com/problems/sqrtx/description/
+    
+
+def mySqrt(x): 
+    if x == 0:
+        return 0
+    else:
+        int, res = 1, 0
+        while res <= x:
+            res = int*int
+            int += 1
+    return (int-2) 
+    
+    
+# Example:
+x = 396
+print(mySqrt(x))
+
+
+
+
+
